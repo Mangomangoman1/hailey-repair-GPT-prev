74b8b3848f3d8bb5c9e6b0e82ca19f546fe0e64b
Mangomangoman1 <159656186+Mangomangoman1@users.noreply.github.com>
2026-03-17 22:37:07 -0600

    glitch header

---
+++ app/components/HeaderTextButtonPlain.tsx
@@ -0,0 +1,7 @@
+export default function HeaderTextButtonPlain() {
+  return (
+    <a className="btn btn-call btn-call--plain" href="sms:+12083666111" aria-label="Text Hailey Device Repair">
+      <span className="btn-call-label">Text</span>
+    </a>
+  )
+}
